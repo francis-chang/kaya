cf72214e782bc0d88a58ea80e57a26b9c6e90034
francis <francis.c@zohomail.com>
2023-01-20 22:03:58 -0800

    wee

--- api/routes/auth/index.ts
+++ api/routes/auth/index.ts
@@ -4,6 +4,7 @@ import passport from '../../utils/passport'
 
 import createUser from './resources/createUser'
 import { findUserAvailable, findEmailAvailable } from './resources/findUser'
+import axios from 'axios'
 
 const authRouter = express.Router()
 
@@ -12,6 +13,34 @@ authRouter.get('/finduser/:username', findUserAvailable)
 authRouter.get('/findemail/:email', findEmailAvailable)
 authRouter.post('/login', auth)
 authRouter.get('/google', passport.authenticate('google', { scope: ['email'] }))
+
+authRouter.get('/sendtestemail', async (req, res) => {
+    try {
+        await axios.post(
+            'https://api.postmarkapp.com/email',
+            {
+                From: 'FTY Fantasy Sports <noreply@fty.gg>',
+                To: 'fcs.kb24@gmail.com',
+                Subject: 'Postmark Test',
+                TextBody: 'Hello deark postmark user.',
+                MessageStream: 'outbound',
+            },
+            {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'X-Postmark-Server-Token': process.env.POSTMARK_API_TOKEN,
+                },
+            }
+        )
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ hello: 'hello' })
+        return
+    }
+    res.status(200).json({ hello: 'hello' })
+    return
+})
 
 function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
     if (req.user) {
